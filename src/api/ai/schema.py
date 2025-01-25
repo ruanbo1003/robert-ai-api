fc79f4acded21b08e63711cb18bc287d78eecea2
Bo Ruan <bo.ruan1@lotuscars.com.cn>
2025-01-25 10:51:51 +0800

    use langchain

--- src/api/ai/schema.py
+++ src/api/ai/schema.py
@@ -2,18 +2,7 @@
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
-    name: str
-
-
-class LlmTranslate(BaseModel):
+# translate
+class AiTranslate(BaseModel):
     model: str
     content: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        orm_mode = True
